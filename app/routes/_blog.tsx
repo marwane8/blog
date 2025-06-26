64d80d3cc5c299a384cf50aa7ed074f6fed94a97
Marwane <taroua.marwane@gmail.com>
2025-06-26 15:27:24 -0400

    creating pretty typefonts about me page

--- app/routes/_blog.tsx
+++ app/routes/_blog.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "@remix-run/react";
 
 export default function Blog() {
   return (
-    <div className="prose p-10 dbug">
+    <div className="prose m-auto max-w-screen-md  py-5">
       <Outlet />
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,9 +19,141 @@ export default {
         cal: ["Caladea"],
         lexend: ["Lexend Deca"],
       },
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      typography: (theme: any) => {
+        const reset = {
+          color: "inherit",
+          h1: {
+            margin: "0",
+            textAlign: "center",
+            fontFamily: theme("fontFamily.lexend").join(", "),
+            color: theme("colors.teal.500"),
+            fontWeight: "800",
+          },
+          h2: {
+            marginTop: "1rem",
+            marginBottom: "1rem",
+            fontFamily: theme("fontFamily.lexend").join(", "),
+            fontSize: theme("fontSize.3xl")[0],
+            [`@media (min-width: ${theme("screens.md")})`]: {
+              fontSize: theme("fontSize.4xl")[0],
+            },
+
+            color: "inherit",
+          },
+
+          h3: {
+            marginTop: ".75rem",
+            marginBottom: ".75rem",
+            fontFamily: theme("fontFamily.lexend").join(", "),
+            color: "inherit",
+            fontSize: theme("fontSize.2xl")[0],
+            [`@media (min-width: ${theme("screens.md")})`]: {
+              fontSize: theme("fontSize.3xl")[0],
+            },
+          },
+          h4: {
+            marginTop: ".75rem",
+            marginBottom: ".75rem",
+            fontFamily: theme("fontFamily.lexend").join(", "),
+            color: "inherit",
+            fontSize: theme("fontSize.lg")[0],
+            [`@media (min-width: ${theme("screens.md")})`]: {
+              fontSize: theme("fontSize.xl")[0],
+            },
+          },
+
+          p: {
+            marginTop: ".75rem",
+            marginBottom: ".75rem",
+            fontFamily: theme("fontFamily.cal").join(", "),
+            fontSize: theme("fontSize.lg")[0],
+            letterSpacing: "0.5px",
+            [`@media (min-width: ${theme("screens.md")})`]: {
+              fontSize: theme("fontSize.xl")[0],
+            },
+          },
+          a: {
+            margin: "0",
+            color: "inherit",
+            textDecoration: "underline",
+            textDecorationColor: theme("colors.teal.500"),
+            textDecorationStyle: "dotted",
+            textUnderlineOffset: "3px",
+            "&:hover": {
+              textDecorationStyle: "solid",
+              color: theme("colors.teal.500"),
+            },
+          },
+          li: {
+            margin: "0",
+          },
+
+          code: {
+            color: theme("colors.teal.500"),
+            backgroundColor: theme("colors.teal.500") + "20",
+            paddingLeft: "5px",
+            paddingRight: "5px",
+            borderRadius: "3px",
+            letterSpacing: "0.005em",
+
+            "&::before": {
+              display: "none",
+            },
+            "&::after": {
+              display: "none",
+            },
+          },
+          ol: {
+            marginTop: ".75rem",
+            marginBottom: ".75rem",
+            fontFamily: theme("fontFamily.cal").join(", "),
+            fontSize: theme("fontSize.lg")[0],
+            letterSpacing: "0.5px",
+            [`@media (min-width: ${theme("screens.md")})`]: {
+              fontSize: theme("fontSize.xl")[0],
+            },
+
+            "li::marker": {
+              color: "inherit",
+            },
+          },
+          ul: {
+            marginTop: ".75rem",
+            marginBottom: ".75rem",
+            fontFamily: theme("fontFamily.cal").join(", "),
+            fontSize: theme("fontSize.lg")[0],
+            letterSpacing: "0.5px",
+            [`@media (min-width: ${theme("screens.md")})`]: {
+              fontSize: theme("fontSize.xl")[0],
+            },
+
+            "li::marker": {
+              color: "inherit",
+            },
+          },
+          pre: {
+            backgroundColor: theme("colors.teal.900"),
+          },
+          figcaption: {
+            margin: "3px",
+            textAlign: "center",
+            fontFamily: theme("fontFamily.cal").join(", "),
+            letterSpacing: theme("letterSpacing.wide"),
+            fontSize: theme("fontSize.lg")[0],
+          },
+        };
+        return {
+          DEFAULT: { css: reset },
+          dark: {
+            css: {
+              ...reset,
+            },
+          },
+        };
+      },
     },
   },
-
   darkMode: "class",
   plugins: [typography],
 } satisfies Config;
